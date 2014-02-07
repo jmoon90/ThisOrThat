ff19ce2073ed2aa17203910e36fe25b010ae8498
John Moon <johnmoonyy@gmail.com>
2014-02-07 18:20:35 -0500

    Initial commit. Add Readme, secret token in gitignore

---
+++ README.md
@@ -0,0 +1,11 @@
+#This OR That
+What product do you like better?
+
+##How-to-play
+Pick and choose what you think is better.
+See what others think
+Create your own comparison
+
+#Feel free to fork, submit pull requests, etc
+Open source project
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ThisOrThat::Application.config.secret_key_base = '6b3f506274cc1b4514568f7b9a22b524a4d876559083bbfeeac789a8ce1199d2952d4d67aae2a5692d7f41973f94f8a8dca098e91b5b82b5dfccfc6340302d7c'

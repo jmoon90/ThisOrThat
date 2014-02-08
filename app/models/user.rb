b2e223c4e031ca545cbb378d92be467fb480225a
John Moon <johnmoonyy@gmail.com>
2014-02-08 11:40:09 -0500

    Add admin field to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,9 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   validates_presence_of :first_name
   validates_presence_of :last_name
+
+
+  def self.admin?
+    self.admin == true
+  end
 end
---
+++ spec/features/admin_user_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+feature 'User' do
+  given(:user) { FactoryGirl.create(:user) }
+  scenario 'is not an admin' do
+    login_as(user, scope: :user)
+    visit statuses_path
+
+    expect(page).to have_content("Must be an admin")
+  end
+
+  scenario 'is an admin' do
+    choice = FactoryGirl.create(:choice)
+    user.update_attributes(admin: true)
+    login_as(user, scope: :user)
+    visit statuses_path
+
+  end
+end

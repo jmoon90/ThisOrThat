2d74761ed25f70df30195216dab292f0d5afcaa8
John Moon <johnmoonyy@gmail.com>
2014-02-07 19:32:48 -0500

    Create choices route, view, and specs

---
+++ spec/features/create_choice_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+feature 'user' do
+  scenario 'creates choices with valid input' do
+    visit new_choice_path
+    fill_in 'Choice1', with: "Pepsi"
+    fill_in 'Choice2', with: "Coke"
+    click_on 'Submit'
+    
+    expect(page).to have_content("You're choice is under view. Thanks for submitting")
+  end
+
+  scenario 'creates choices with invalid input' do
+    visit new_choice_path
+    click_on 'Submit'
+    expect(page).to have_content("Invalid input. Please try again")
+  end
+end

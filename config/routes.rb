bf79e1317be5348a12fc13022699dc92a1afb3b3
John Moon <johnmoonyy@gmail.com>
2014-02-09 20:32:30 -0500

    Delete choice index.
    We will be showing 1 choice pair at a time.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 ThisOrThat::Application.routes.draw do
   devise_for :users
-  root 'choices#index'
-  resources :choices
-
+  root 'choices#show'
+  resources :choices, except: [:index]
   resources :statuses, only: [:index, :update, :destroy]
+  resources :vote, only: [:update]
 end
--- spec/features/vote_on_choices_spec.rb
+++ spec/features/vote_on_choices_spec.rb
@@ -4,12 +4,17 @@ feature 'User' do
   scenario 'views choices' do
     status = FactoryGirl.create(:status)
     status.update_attributes(approved: true, pending: false)
-    visit choices_path
+    visit root_path
 
     expect(page).to have_content(status.choice.choice1)
   end
 
   scenario 'votes between choices' do
-    pending
+    status = FactoryGirl.create(:status)
+    status.update_attributes(approved: true, pending: false)
+    visit root_path
+
+    click_on status.choice.choice1
+    expect(page).to have_content('agree')
   end
 end
